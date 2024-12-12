a389ad54722438ef339f3019172b335dcd284dbe
Mezzofloat <skull.wizard.2299@gmail.com>
2024-12-12 01:07:58 -0500

    refactored anf rewrote

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.IO;
 
 using StreamReader reader = new("../../../input.txt");
 string text = reader.ReadToEnd();
@@ -6,6 +7,15 @@ string text = reader.ReadToEnd();
 string[] reports = text.Split('\n');
 
 int safeReports = 0;
+
+bool EvaluateLevelSafety(int j1, int j2, bool sequenceDecreasing) {
+    int delta = j2 - j1;
+    bool gradual = Math.Abs(delta) >= 1 && Math.Abs(delta) <= 3;
+    bool continuing = delta < 0 == sequenceDecreasing;
+
+    return gradual && continuing;
+}
+
 foreach (string report in reports) {
     string[] s = report.Split(' ');
     (int j1, int j2) = (int.Parse(s[0]), int.Parse(s[1]));
@@ -15,15 +25,12 @@ foreach (string report in reports) {
     }
 
     bool sequenceIsDecreasing = (j2 - j1) < 0;
+    int unsafeLevels = 0;
     for (int i = 1; i < s.Length - 1; i++) {
-        (j1, j2) = (int.Parse(s[i]), int.Parse(s[i+1]));
-        int deltaj = j2 - j1;
-        if (deltaj < 0 != sequenceIsDecreasing) {
+        if (!EvaluateLevelSafety(int.Parse(s[i]), int.Parse(s[i+1]), sequenceIsDecreasing)) {
             break;
         }
-        if (Math.Abs(deltaj) > 3 || Math.Abs(deltaj) < 1) {
-            break;
-        }
+
         if (i == s.Length - 2) {
             safeReports++;
         }
